ee8e603c8ff73e789d9510d50ec8de537259f2fc
sebastian-montero <smonteroparis@icloud.com>
2024-12-01 23:53:27 +0000

    fmt

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -65,23 +65,23 @@ func main() {
 // It allows requests from the localhost.
 // It also sets up the allowed methods and headers for the server.
 func withCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        origin := r.Header.Get("Origin")
-        allowedOrigin := "https://haiku.incendia.dev"
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		allowedOrigin := "https://haiku.incendia.dev"
 
-        if origin == allowedOrigin {
-            w.Header().Set("Access-Control-Allow-Origin", origin)
-        }
+		if origin == allowedOrigin {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 
-        w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-        if r.Method == http.MethodOptions {
-            w.WriteHeader(http.StatusNoContent)
-            return
-        }
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
