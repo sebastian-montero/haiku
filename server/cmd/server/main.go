a1a043c14542434bb163ea2d72a870ed96231916
sebastian-montero <smonteroparis@icloud.com>
2024-12-01 21:45:02 +0000

    add cors

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -66,8 +66,8 @@ func main() {
 // It also sets up the allowed methods and headers for the server.
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "https://haiku.incendia.dev/")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
